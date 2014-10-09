6fe53091bf661fd70f9e2b65b41f44a8f555fe97
lingtzar <lingtzar@gmail.com>
2014-10-09 14:45:18 +0400

    add-energy improved

--- Fourier.cpp
+++ Fourier.cpp
@@ -8,7 +8,7 @@ buffer buf[2];
 
 
 double *imp, *spec, energy, E_0, step;
-int implen, speclen;
+int implen, len;
 
 enum functions
 {
@@ -23,45 +23,45 @@ void function(functions func, int left_b, int right_b){
 
 	if (func == FUNC_Sine){
 		for (int i = left_b; i < right_b; i++){
-			in[i] = sin(i-left_b);
+			buf.imp[i] = sin(i-left_b);
 		}
 	}
 
 	if (func == FUNC_None){
 		for (int i = left_b; i < right_b; i++){
-			in[i] = 0;
+			buf.imp[i] = 0;
 		}
 	}
 
 	if (func == FUNC_Linear){
 		for (int i = left_b; i < right_b; i++){
-			in[i] = (-(i-right_b)) * (left_b-right_b);
+			buf.imp[i] = (-(i-right_b)) * (left_b-right_b);
 		}
 	}
 
 	if (func == FUNC_Gauss){
 		for (int i = left_b; i < right_b; i++){
-			in[i] = exp(-((i-left_b)/parameter)^2);
+			buf.imp[i] = exp(-((i-left_b)/parameter)^2);
 		}
 	}
 
 	if (func == FUNC_Jump){
 		for (int i = left_b; i < right_b; i++){
-			in[i] = 0.0;
+			buf.imp[i] = 0.0;
 		}
 	}
 
 }
 
-void alloc(int size){
+void alloc(){
 
 	static fftw_plan p1,p2;
 
-	p1 = fftw_plan_dft_r2c_1d(speclen, imp, out, FFTW_ESTIMATE);
+	p1 = fftw_plan_dft_r2c_1d(buf.len, buf.imp, out, FFTW_ESTIMATE);
 
-	p2 = fftw_plan_dft_r2c_1d(speclen, imp, out, FFTW_ESTIMATE);
+	p2 = fftw_plan_dft_r2c_1d(buf.len, buf.imp, out, FFTW_ESTIMATE);
 
-	out = (fftw_complex*)fftw_malloc(sizeof(fftw_complex)*(size));
+	out = (fftw_complex*)fftw_malloc(sizeof(fftw_complex)*(buf.len));
 }
 
 
@@ -70,16 +70,16 @@ void fourier(){
 
 	fftw_execute(p1);
 
-	for(i = 0; i < size/2 + 1; i++){
+	for(i = 0; i < buf.len/2 + 1; i++){
 
-		spec[i] = (sqrt(out[i][0]*out[i][0]+out[i][1]*out[i][1]))/size;
+		buf.spec[i] = (sqrt(out[i][0]*out[i][0]+out[i][1]*out[i][1]))/size;
 	}
 
 	fftw_execute(p2);
 
-	for(i = 0; i < size/2 + 1; i++){
+	for(i = 0; i < buf.len/2 + 1; i++){
 
-		spec[i] = (sqrt(out[i][0]*out[i][0]+out[i][1]*out[i][1]))/size;
+		buf.[i] = (sqrt(out[i][0]*out[i][0]+out[i][1]*out[i][1]))/size;
 	}
 
 	fftw_free(out);
@@ -90,18 +90,18 @@ void add_energy(){
 	int length = (E_0 - energy)/step;
 	for(int i = 0; i < size/2; i++){
 		if ((i + length/2) < size/2){
-			imp[i] = imp[i+length/2];
+			buf.imp[i] = buf.imp[i+length/2];
 		}
 	}
 
 	for(int i = size; i > size/2; i--){
 		if ((i - length/2) > size/2){
-			imp[i] = imp[i-length/2];
+			buf.imp[i] = buf.imp[i-length/2];
 		}
 	}
 
 	for(int i = (size/2 - length/2); i < (size/2 + length/2); i++){
-		imp[i] = 1.0;
+		buf.imp[i] = 1.0;
 	}
 
 
@@ -112,8 +112,8 @@ void check_energy(){
 
 	energy = 0.0;
 
-	for (int i = 0; i < size; i++){
-		energy += imp[i]*imp[i];
+	for (int i = 0; i < buf.len; i++){
+		energy += buf.imp[i]*buf.imp[i];
 	}
 
 	if ((E_0 - energy) > 0){
