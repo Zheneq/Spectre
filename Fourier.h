f4887d73f8bbf6ea775436f4fcc92dbf0294b58a
Zhenёq <evgeniyspam@ya.ru>
2014-12-24 05:39:20 +0300

    ***

--- Fourier.h
+++ Fourier.h
@@ -92,6 +92,14 @@ struct buffer
 		}
 		std::cout << "     " << tt << '/' << Level * SpecSqrSum << " (" << Level << " of " << SpecSqrSum << ")" << '\n';
 
+		/////////////////////////////
+/*
+// Хранить в спеке квадрат (для отладки)
+		for (int i = 0; i <= len/2; ++i)
+		{
+			spec[i] *= spec[i];
+		}
+*/
 	}
 
 	void check_energy(){
